5a693069be3d1d5adb9a24b2dea50c478ee3a567
Gabrielinaki <ig.coronel01@gmial.com>
2022-09-25 18:31:46 -0300

    modificacion de poo

--- poo/src/EjemploAutomovilEnum.java
+++ poo/src/EjemploAutomovilEnum.java
@@ -20,13 +20,20 @@ public class EjemploAutomovilEnum {
         tipo = mazda.getTipo();
 
         switch(tipo){
-            case CONVERTIBLE -> System.out.println("El automóvil es deportivo y descapotable de dos puertas");
-            case COUPE -> System.out.println("Es un automóvil pequeño de dos puertas y típicamente deportivo");
-            case FURGON -> System.out.println("Es un automóvil  utilitario de transporte, de empresa");
-            case HATCHBACK -> System.out.println("Es un automóvil mediano compacto, aspecto deportivo");
-            case PICKUP -> System.out.println("Es un automóvil de doble cabina o camioneta");
-            case SEDAN -> System.out.println("Es un automóvil mediano");
-            case STATION_WAGON -> System.out.println("Es un automóvil mas grande, con maleta grande...");
+            case CONVERTIBLE:
+                System.out.println("El automóvil es deportivo y descapotable de dos puertas");
+            case COUPE:
+                System.out.println("Es un automóvil pequeño de dos puertas y típicamente deportivo");
+            case FURGON :
+                System.out.println("Es un automóvil  utilitario de transporte, de empresa");
+            case HATCHBACK:
+                System.out.println("Es un automóvil mediano compacto, aspecto deportivo");
+            case PICKUP:
+                System.out.println("Es un automóvil de doble cabina o camioneta");
+            case SEDAN:
+                System.out.println("Es un automóvil mediano");
+            case STATION_WAGON:
+                System.out.println("Es un automóvil mas grande, con maleta grande...");
         }
 
         TipoAutomovil[] tipos = TipoAutomovil.values();
